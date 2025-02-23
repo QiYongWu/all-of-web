e6084bca56eafcc18dd900a2f9e4d865ee1a3753
Wu Yong Qi <w18178865609@outlook.com>
2025-02-23 21:32:34 +0800

    2-23学习了很多Vue3

---
+++ Vue3/Vue3_For_Study/src/components/my-components/start.vue
@@ -0,0 +1,36 @@
+<script>
+export default{
+    name : 'start',
+    data:function(){
+    return {
+        name : 'Vue3',
+        age : 6
+    };
+   },
+
+   methods: {
+    showInfo() {
+      // alert 只能接收一个参数，建议拼接字符串
+      alert(this.name + ', ' + this.age);
+    },
+    showName() {
+      alert(this.name);
+    }
+  }
+}
+   
+
+</script>
+
+<template>
+    
+    <p>Hello {{ name }}</p>
+    <p>age : {{ age }}</p>
+
+    <button v-on:click = 'showInfo'>点我打印信息</button>
+    <button v-on:click = 'showName'>点我打印名字</button>
+    
+</template>
+
+<style>
+</style>
